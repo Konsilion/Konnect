5e8355684707de76184a82f0e7d2b33ef1c5c64b
nicolasbremond <nicolasbremond@konsilion.fr>
2022-08-12 12:07:35 +0200

    add version konnect

--- Konnect/server/etc/konnect.sh
+++ Konnect/server/etc/konnect.sh
@@ -33,11 +33,13 @@ clear
 
 function konnect {
 
+local version="1.0"
+
 # COMMENTAIRES : La page principale Konnect présente les principales actions possible avec le HUB.
 
 # MODIFIABLE - Paramètres de votre page home
 
-	local page_name="ACCUEIL - 1.0.0"
+	local page_name="ACCUEIL"
 	local page_color=${BLUE}
 	local page_bck_color=${BCK_CYAN}
 	local page_master="konnect"
@@ -46,7 +48,7 @@ function konnect {
 
 	conda_env_verif
 	
-	clear -x && echo -ne "\e]0;${HUB_NAME} - ${page_name}\a"
+	clear -x && echo -ne "\e]0;${HUB_NAME} - ${page_name} - ${version}\a"
 	
 	ksln_header "" ${page_name} ${page_bck_color} 
 	
