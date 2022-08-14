54c756e8ff931c4445c4b32cce567f64e5c1a4a0
nicolasbremond <nicolasbremond@konsilion.fr>
2022-08-14 11:53:17 +0200

    add winpath.sh

---
+++ Konnect/server/etc/winpath.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+function _winpath {
+
+	echo "$@" | sed -e 's|\\|/|g' -e 's|^\([A-Za-z]\)\:/\(.*\)|/mnt/\L\1\E/\2|'
+
+}
+
+function winpath {
+
+	path="$(_winpath $1)"
+	
+	echo $path
+
+} 
+
+
